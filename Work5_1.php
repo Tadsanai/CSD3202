d6f57e57cc4c088cb8f8dd2823e46a99f0ee18af
dewwss <123717960+dewwss@users.noreply.github.com>
2024-09-06 00:03:44 +0700

    Create Work5_1.php

---
+++ Work5_1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h3>เขียนโปรแกรมที่ประกาศตัวแปร 3 ชนิด </h3>
+    <h3>Name: ทรรศนัย สวยล้ำ ID: 65122250031</h3>
+    <hr>
+
+    <?php
+    $name = "ทรรศนัย สวยล้ำ";
+    $age = 21;
+    $height = 1.75;
+
+    echo "Name: $name <br>";
+    echo "Age: $age <br>";
+    echo "Height: $height";
+    ?>
+</body>
+
+</html>
